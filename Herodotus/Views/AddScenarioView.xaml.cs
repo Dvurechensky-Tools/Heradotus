108cc415355fa8e05aaa97df7c38784773abbdb0
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-29 06:53:29 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-29

--- Herodotus/Views/AddScenarioView.xaml.cs
+++ Herodotus/Views/AddScenarioView.xaml.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 января 2026 06:53:31
- * Version: 1.0.91
+ * Last Updated: 29 января 2026 06:53:27
+ * Version: 1.0.92
  */
 
 using System;
