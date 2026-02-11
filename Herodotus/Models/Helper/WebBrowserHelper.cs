7fc0223e638719a083c210bd3ab9cb47ff3bba7b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-11 09:15:44 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-11

--- Herodotus/Models/Helper/WebBrowserHelper.cs
+++ Herodotus/Models/Helper/WebBrowserHelper.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 февраля 2026 10:34:48
- * Version: 1.0.104
+ * Last Updated: 11 февраля 2026 09:15:42
+ * Version: 1.0.105
  */
 
 using System.Windows;
