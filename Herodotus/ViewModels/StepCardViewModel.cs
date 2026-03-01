2a2aa18b75ac1d27157e22fc1aad57a1ee0b748f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-01 16:38:18 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-01

--- Herodotus/ViewModels/StepCardViewModel.cs
+++ Herodotus/ViewModels/StepCardViewModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 февраля 2026 15:57:00
- * Version: 1.0.122
+ * Last Updated: 01 марта 2026 16:38:16
+ * Version: 1.0.123
  */
 
 using System;
