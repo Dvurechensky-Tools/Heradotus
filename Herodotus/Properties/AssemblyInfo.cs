5d4745b02171f420efa953c60691d1b792795895
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-08 09:57:48 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-08

--- Herodotus/Properties/AssemblyInfo.cs
+++ Herodotus/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 марта 2026 06:53:43
- * Version: 1.0.129
+ * Last Updated: 08 марта 2026 09:57:46
+ * Version: 1.0.130
  */
 
 using System.Reflection;
