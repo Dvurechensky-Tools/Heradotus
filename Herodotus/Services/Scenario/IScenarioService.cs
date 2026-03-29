d97dd580ce7b2b918b7990460e1d9f87fb4abf6d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-29 11:14:40 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-29

--- Herodotus/Services/Scenario/IScenarioService.cs
+++ Herodotus/Services/Scenario/IScenarioService.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 марта 2026 10:13:02
- * Version: 1.0.150
+ * Last Updated: 29 марта 2026 11:14:38
+ * Version: 1.0.151
  */
 
 using System.Collections.ObjectModel;
