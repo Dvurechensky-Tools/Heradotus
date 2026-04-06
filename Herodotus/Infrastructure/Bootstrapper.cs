d53d333e3afa36f952e05474e749b9592481d808
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-06 13:04:26 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-06

--- Herodotus/Infrastructure/Bootstrapper.cs
+++ Herodotus/Infrastructure/Bootstrapper.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 апреля 2026 15:18:37
- * Version: 1.0.158
+ * Last Updated: 06 апреля 2026 13:04:24
+ * Version: 1.0.159
  */
 
 using Prism.DryIoc;
