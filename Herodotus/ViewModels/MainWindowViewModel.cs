5904f237f2de21b0dbd8abce4cb8675c4a7ac7b3
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-09 11:14:37 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-09

--- Herodotus/ViewModels/MainWindowViewModel.cs
+++ Herodotus/ViewModels/MainWindowViewModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 апреля 2026 14:44:44
- * Version: 1.0.161
+ * Last Updated: 09 апреля 2026 11:14:35
+ * Version: 1.0.162
  */
 
 using System;
