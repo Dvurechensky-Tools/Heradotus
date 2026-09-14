9f974d314babeadbd2180c1e29069403f6b626a5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-14 09:57:13 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-14

--- Herodotus/Models/StepItemModel.cs
+++ Herodotus/Models/StepItemModel.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 сентября 2026 06:58:14
- * Version: 1.0.318
+ * Last Updated: 14 сентября 2026 09:57:11
+ * Version: 1.0.319
  */
 
 using Prism.Mvvm;
